d03d6a1c8662219b4c86b863b565abf0e2003f66
Anjay Kannan <anjaykannan508@gmail.com>
2026-05-11 09:25:50 -0400

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/FBLA-Business-Management-Nat-Prep/',
   plugins: [
     react(),
     tailwindcss(),
